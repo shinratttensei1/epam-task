4692cb912788c73a13c26baa5aadc3d52f45d8e2
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2025-01-27 21:31:28 +0500

    fixed test2

--- task04/main.tf
+++ task04/main.tf
@@ -76,6 +76,11 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 data "azurerm_image" "ubuntu" {
   name                = var.vm_os_version
   resource_group_name = azurerm_resource_group.rg.name
