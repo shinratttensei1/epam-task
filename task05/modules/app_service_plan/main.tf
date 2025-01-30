de6b0717562ec901cdf6a7662e5e4ab4f68f178a
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2025-01-31 02:33:42 +0500

    task05 fixed

--- task05/modules/app_service_plan/main.tf
+++ task05/modules/app_service_plan/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = var.os_type
-  sku_name            = var.sku_name   
+  sku_name            = var.sku_name
   worker_count        = var.worker_count
   tags                = var.tags
 }
