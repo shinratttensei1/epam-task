d9d9644abdfb89799d1a0a872aa89bc2c2d90ac3
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2025-02-05 03:53:06 +0500

    task06 added

--- task06/locals.tf
+++ task06/locals.tf
@@ -1,9 +1,9 @@
 locals {
   prefix = "cmaz-cc456562-mod6"
 
-  rg_name          = format("%s-rg", local.prefix)
-  sql_server_name  = format("%s-sql", local.prefix)
-  sql_db_name      = format("%s-sql-db", local.prefix)
-  asp_name         = format("%s-asp", local.prefix)
-  app_name         = format("%s-app", local.prefix)
+  rg_name         = format("%s-rg", local.prefix)
+  sql_server_name = format("%s-sql", local.prefix)
+  sql_db_name     = format("%s-sql-db", local.prefix)
+  asp_name        = format("%s-asp", local.prefix)
+  app_name        = format("%s-app", local.prefix)
 }
