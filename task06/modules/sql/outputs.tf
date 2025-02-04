4f64adece222540ed10a3cbd43e831d5ea280b54
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2025-02-05 03:31:55 +0500

    task06 added

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,3 +1,7 @@
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.sql_server.fully_qualified_domain_name
+}
+
 output "sql_connection_string" {
   value     = sensitive("Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_db.name};Persist Security Info=False;User ID=sqladmin;Password=${random_password.password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
   sensitive = true
