cfe48439342102e74ef07f74b878bb7b87c673ac
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2025-02-05 01:04:16 +0500

    task06 added

--- task06/modules/sql/main.tf
+++ task06/modules/sql/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
 resource "azurerm_mssql_firewall_rule" "allow_specific_ip" {
   name             = "Allow-Specific-IP"
   server_id        = azurerm_mssql_server.sql_server.id
-  start_ip_address = var.allowed_ip_address 
+  start_ip_address = var.allowed_ip_address
   end_ip_address   = var.allowed_ip_address
 }
 
