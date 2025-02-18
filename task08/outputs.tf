e26076ac8d6e18ad4f0bbc178e462c40c6efe1fa
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2025-02-18 20:49:33 +0500

    fixed

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -3,7 +3,3 @@ output "aci_fqdn" {
   value       = module.aci.fqdn
 }
 
-output "aks_lb_ip" {
-  description = "Load Balancer IP address of the application running on AKS"
-  value       = try(jsondecode(jsonencode(data.kubernetes_service.app_service.status)).load_balancer.ingress[0].ip, null)
-}
